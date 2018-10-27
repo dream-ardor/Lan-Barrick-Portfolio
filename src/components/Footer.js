47c1f98af6ff90578edc9c6001f7a7234f306500
Dream-Ardor <barrick.dan80@gmail.com>
2018-10-27 15:56:53 -0700

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy;Dan Barrick Design. Made With Love By: <a href="https://www.linkedin.com/in/danbarrick"><i class="fas fa-heart"></i>Dan Barrick</a></p>
+        <p className="copyright">&copy;Dan Barrick Design. Made With Love By: <a href="https://www.linkedin.com/in/danbarrick"><p className=" icon fa-heart"></i>Dan Barrick</a></p>
     </footer>
 )
 
